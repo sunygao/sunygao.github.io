28c7e3c322472846a7dd70f3d355513ba36deb08
Suny Gao <suny.gao@gmail.com>
2021-11-23 19:38:56 -0500

    update css path

--- toy/lokyin/global.js
+++ toy/lokyin/global.js
@@ -3,6 +3,7 @@ var land = 0;
 var number = 0;
 var countNum = 0;
 var layer = -1;
+var allowFlips = true
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -13,35 +14,45 @@ function getRandomInt(min, max) {
 this.document.addEventListener("keydown", space)
 
 function space(e) {
-    if(e.keyCode === 32) {       	        	
+    if(allowFlips === false) return
+
+    if(e.keyCode === 32) {  
+        allowFlips = false     	        	
 
         startKeys = 1;
         layer++;
         number = (getRandomInt(1, 10));
         document.getElementById("number").innerHTML = number;
-        if(number <= 7) {
-            document.getElementById("land").innerHTML = "fail!";
-            land = 0;
-            console.log(land)
-            document.getElementById("bottleFailContainer").style.zIndex = layer;
-            var failVideo = document.getElementById("bottleFail")
-            failVideo.load()
-            failVideo.play()
-        }
-        else {
+        // if(number <= 7) {
+        //     document.getElementById("land").innerHTML = "fail!";
+        //     land = 0;
+        //     console.log(land)
+        //     document.getElementById("bottleFailContainer").style.zIndex = layer;
+        //     var failVideo = document.getElementById("bottleFail")
+        //     failVideo.load()
+        //     failVideo.play()
+        // }
+        // else {
             document.getElementById("land").innerHTML = "land!";
 
             layer++;
-            countNum++;
+           
             // console.log(countNum);
-            document.getElementById("count_key").innerHTML = countNum;
+           
             land = 1;
             console.log(land)
             document.getElementById("bottleLandContainer").style.zIndex = layer;
             var landVideo = document.getElementById("bottleLand")
             landVideo.load()
             landVideo.play()
-        }
+
+            setTimeout(function() {
+                countNum++;
+                document.getElementById("count_key").innerHTML = countNum;
+                allowFlips = true;
+            }, 2000)
+            
+        //}
         // console.log("startKey Inits"); 
     }
 
